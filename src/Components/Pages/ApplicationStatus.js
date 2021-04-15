ad1ce63300583edde173b1c0719859983e12aae9
dragonman164 <sanidhiyafirefox123@gmail.com>
2021-04-15 19:17:27 +0530

    Application Status

--- src/Components/Pages/ApplicationStatus.js
+++ src/Components/Pages/ApplicationStatus.js
@@ -1,15 +1,44 @@
 import ApplicationStatusCard from '../ApplicationStatusCard';
 
+let CompanyDetails = [
+    {
+        company:'abc',
+        status:'Accepted',
+        email: 'abc@gmail.com',
+        contact:'1234567890',
+
+    },
+    {
+        company:'abc',
+        status:'Rejected',
+        email: 'abc@gmail.com',
+        contact:'1234567890',
+
+    },
+    {
+        company:'abc',
+        status:'In Review',
+        email: 'abc@gmail.com',
+        contact:'1234567890',
+
+    }
+]
+
+
 const ApplicationStatus = ()=>{
     return (
         <>
         
         <div className="container bg-dark text-light py-5 my-5">
             <h1>You Have Applied for Following Companies:</h1>
-            
-      <ApplicationStatusCard/>
-      <ApplicationStatusCard/>
-      <ApplicationStatusCard/>
+        {
+            CompanyDetails.map((elem)=>{
+                return (
+                <ApplicationStatusCard company={elem.company} status={elem.status} email={elem.email} contact={elem.contact}/>
+                )
+            })
+        }
+      
           
       
       </div>
